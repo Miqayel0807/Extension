29602d88dfd59282d6a4aa66ec3e47b2b8b780a1
Miqayel0807 <grigoryanmiqayel509@gmail.com>
2021-11-30 00:11:34 +0400

    facebook-extension

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,10 @@
 const init = function () {
-const firstElement= document.querySelector('div')
-  const element = document.createElement("h1");
-  element.className = "welcome";
-  element.innerHTML = "Welcome"
-  firstElement.prepend(element)
+    const firstElement = document.querySelector('div')
+    const element = document.createElement("iframe");
+    element.src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FMeta&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=681387392879540"
+    element.allowFullscreen = true
+    element.className = "facebook";
+    firstElement.prepend(element)
 };
 
 init()
